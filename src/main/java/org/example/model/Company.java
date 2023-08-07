d39b6a580357ccb1b4f5d6d9a372c8d0a575fe4e
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-07 13:36:27 +0300

    quick refactor

--- src/main/java/org/example/model/Company.java
+++ src/main/java/org/example/model/Company.java
@@ -5,26 +5,44 @@ import java.util.Optional;
 
 public class Company {
     private final Map<Integer, Project> projects;
+
     public Company(Map<Integer, Project> projects) {
         this.projects = projects;
     }
 
+
+
     public Optional<MaxEmploymentPair> findMaximumEmploymentPeriodPairBetweenTwoEmployeesOnAProject() {
         MaxEmploymentPair maxEmploymentPair = null;
 
-        for(Integer projectId : projects.keySet()){
+        for (Integer projectId : projects.keySet()) {
 
             final Project project = projects.get(projectId);
 
-            if(project.atLeastTwoPeopleWorkedOnThisProject()) {
+            if (project.atLeastTwoPeopleWorkedOnThisProject()) {
 
                 final Optional<MaxEmploymentPair> maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject = project.findMaximumEmploymentPeriodBetweenTwoEmployeesOnThisProject();
 
-                if(maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject.isPresent()){
-                    maxEmploymentPair = maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject.get();
+                if (maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject.isPresent()) {
+
+                    if (firstEmployeePairFound(maxEmploymentPair)) {
+                        maxEmploymentPair = maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject.get();
+                        continue;
+                    }
+                    if (newLongerEmploymentPairFound(maxEmploymentPair, maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject)) {
+                        maxEmploymentPair = maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject.get();
+                    }
                 }
             }
         }
         return Optional.of(maxEmploymentPair);
+    }
+
+    private boolean newLongerEmploymentPairFound(MaxEmploymentPair maxEmploymentPair, Optional<MaxEmploymentPair> maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject) {
+        return maxEmploymentPair != null && maximumEmploymentPeriodBetweenTwoEmployeesOnThisProject.get().getMaxOverlapMonthsOnProject() > maxEmploymentPair.getMaxOverlapMonthsOnProject();
+    }
+
+    private boolean firstEmployeePairFound(MaxEmploymentPair maxEmploymentPair) {
+        return maxEmploymentPair == null;
     }
 }
